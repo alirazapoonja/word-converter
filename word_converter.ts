6135774319930a8c5e36748dbf4e1c70548b9733
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-04-20 11:20:49 +0500

    create word count project in github and node.js

---
+++ word_converter.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer"
+
+let user_ans = await inquirer.prompt({
+    type: "input",
+    name: "words",
+    message: "Please enter your sentences"
+    })
+
+    let word_count = user_ans.words.trim().split(" ").length
+
+    console.log(`Your sentences has ${word_count} words.`);
